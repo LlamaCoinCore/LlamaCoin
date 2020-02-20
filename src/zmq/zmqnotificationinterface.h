bd5065ef05918aeaa59cbcfac81e819f74986792
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2020-02-20 22:04:37 +0000

    initial

--- src/zmq/zmqnotificationinterface.h
+++ src/zmq/zmqnotificationinterface.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2015-2019 The Bitcoin Core developers
+// Copyright (c) 2015-2019 The Llamacoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
-#define BITCOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
+#ifndef LLAMACOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
+#define LLAMACOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
 
 #include <validationinterface.h>
 #include <list>
@@ -39,4 +39,4 @@ private:
 
 extern CZMQNotificationInterface* g_zmq_notification_interface;
 
-#endif // BITCOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
+#endif // LLAMACOIN_ZMQ_ZMQNOTIFICATIONINTERFACE_H
